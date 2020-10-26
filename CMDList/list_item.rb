a1084ce345b7c50a9b330d6244c7b1c34e4ad5c7
lukehayes <lukepurplehayes@gmail.com>
2020-10-26 15:57:58 +0000

    Added attr_reader for ListItem properties

--- CMDList/list_item.rb
+++ CMDList/list_item.rb
@@ -1,8 +1,17 @@
-
 class ListItem
+
+  attr_reader :task,
+              :created_at,
+              :completed
+  
   def initialize(task, created_at)
     @task = task
     @created_at = created_at
     @completed = false
   end
+
+  def to_s
+    return @task + " | " + @created_at.to_s
+  end
+
 end
