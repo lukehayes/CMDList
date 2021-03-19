27af6ebfbea55746723a4b42582f899e186a6373
lukehayes <lukepurplehayes@gmail.com>
2021-03-19 12:58:27 +0000

    Added BTLR::Colors utility functions for a nicer terminal output

---
+++ BTLR/colors.rb
@@ -0,0 +1,29 @@
+# Module contains helper functions return ANSI escape
+# codes that change the output color/look of text.
+module Color
+
+  def self.clear
+    "\e[00;00m"
+  end
+
+  def self.red(str)
+    "\e[00;31m #{str} #{clear}"
+  end
+
+  def self.green(str)
+    "\e[00;32m #{str} #{clear}"
+  end
+
+  def self.yellow(str)
+    "\e[00;33m #{str} #{clear}"
+  end
+
+  def self.blue(str)
+    "\e[00;34m #{str} #{clear}"
+  end
+
+  def self.strike(str)
+    "\e[00;9m #{str}  #{clear}"
+  end
+
+end
