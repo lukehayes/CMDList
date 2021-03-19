c1155bd54e89734a879633019549384ece368363
lukehayes <lukepurplehayes@gmail.com>
2021-03-19 17:53:36 +0000

    app.rb now requires colors.rb

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@
 require "./BTLR/list.rb"
 require "./BTLR/list-item.rb"
 require "./BTLR/config.rb"
+require "./BTLR/colors.rb"
 
 # Create a config here so that BTLR has access
 # to everything that it needs from the start.
@@ -11,10 +12,8 @@ config = BTLR::Config.new
 l = BTLR::List.new
 li = BTLR::ListItem.new("A goal to hit")
 
-
 task_str = "Task: #{li.task} :: #{li.created_at.to_s} :: #{li.completed} \n\n"
 
 # Write the contents of the task to a file.
 File.write(config.default_filename, li, mode:"a")
-
 
