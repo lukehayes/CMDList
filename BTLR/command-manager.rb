1500fdc2c316f3f5418172cb7f1d2e61006d0986
lukehayes <lukepurplehayes@gmail.com>
2021-03-22 11:36:44 +0000

    Added BTLR::CommandManager class

---
+++ BTLR/command-manager.rb
@@ -0,0 +1,11 @@
+module BTLR
+  # Class is a manager that deals with the input on
+  # the command line from the user and interperets
+  # it for use inside BTLR.
+  class CommandManger
+
+    def initialize()
+    end
+    
+  end
+end
