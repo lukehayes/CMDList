ead8d762ceec7f1f9ffef1bdb2728e9091592413
lukehayes <lukepurplehayes@gmail.com>
2021-03-22 12:55:52 +0000

    Added BTLR::Exception::CommandError Exception class

---
+++ BTLR/Exception/command_error.rb
@@ -0,0 +1,11 @@
+module BTLR::Exception
+  # Custom exception for dealing with poorly formatted
+  # arguments sent in from the command line.
+  class CommandError < StandardError
+    def initialize(msg="Incorrect command format. Try the -h flag to see the options.", 
+                   exception_type="Command Error")
+      @exception_type = exception_type
+      super(msg)
+    end
+  end
+end
