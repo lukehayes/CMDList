bd0d1b10bc2eae4a46c300d1c0edf40ef5bb9114
lukehayes <lukepurplehayes@gmail.com>
2021-03-23 15:57:41 +0000

    ListManager::show_all method now working. Needs refactoring, but its working.

--- BTLR/list_manager.rb
+++ BTLR/list_manager.rb
@@ -1,14 +1,13 @@
 module BTLR
   class ListManager
 
-    def initialize()
+    def initialize(config)
+      @config = config
     end
 
     # Show all of the lasts that are stored inside the list.
     def show_all
-      config = Config.new()
-      File.read(config.default_filename)
+      File.read(@config.default_filename).each_line {|line| puts line }
     end
-    
   end
 end
--- app.rb
+++ app.rb
@@ -6,11 +6,17 @@ Dir.glob("BTLR/**/*.rb") {|file| require_relative file }
 # Create a config here so that BTLR has access
 # to everything that it needs from the start.
 config = BTLR::Config.new
+list = BTLR::ListManager.new(config)
 
 # Setup a CommandManager to manage incoming options flags.
 manager = BTLR::CommandManager.new
-opt = manager.command(config)
+opt = manager.manage(config)
 
 
-
+# Check if the option is available, then call it.
+if list.respond_to? opt['method']
+  list.send(opt['method'])
+else
+  raise BTLR::Exception::CommandError.new
+end
 
