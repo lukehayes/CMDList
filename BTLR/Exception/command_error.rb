f504e52c4889aa640c07cb460386a3409bb69d8d
lukehayes <lukepurplehayes@gmail.com>
2021-03-23 11:43:41 +0000

    Previous module syntax not working in 3.0.0. Updated and now works.

--- BTLR/Exception/command_error.rb
+++ BTLR/Exception/command_error.rb
@@ -1,11 +1,13 @@
-module BTLR::Exception
-  # Custom exception for dealing with poorly formatted
-  # arguments sent in from the command line.
-  class CommandError < StandardError
-    def initialize(msg="Incorrect command format. Try the -h flag to see the options.", 
-                   exception_type="Command Error")
-      @exception_type = exception_type
-      super(msg)
+module BTLR
+  module Exception
+    # Custom exception for dealing with poorly formatted
+    # arguments sent in from the command line.
+    class CommandError < StandardError
+      def initialize(msg="Incorrect command format. Try the -h flag to see the options.", 
+                     exception_type="Command Error")
+        @exception_type = exception_type
+        super(msg)
+      end
     end
   end
 end
